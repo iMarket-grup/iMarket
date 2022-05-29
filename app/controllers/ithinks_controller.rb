268cb51132fa7254fb49def38c15b41d543342a1
Baytambay <salim.tambay@iwallet.com.tr>
2022-05-29 18:09:55 +0300

    ithink oluşturuldu

--- app/controllers/ithinks_controller.rb
+++ app/controllers/ithinks_controller.rb
@@ -1,4 +1,17 @@
 class IthinksController < ApplicationController
   def index
+    @arr = ['fa-clock', 'fa-comments', 'fa-user', 'fa-camera', 'fa-video','fa-city','fa-slash','fa-star']
+
+    @ithink = Ithink.new 
+    @ithinks = Ithink.all
+    @colour = "%06x" % (rand * 0xffffff)
+  end
+  def create
+    ithink  = Ithink.create!(ithink_params)
+    redirect_to ithinks_path
+  end
+
+  def ithink_params
+    params.require(:ithink).permit(:name, :surname, :content)
   end
 end
